a8df55da3a2332caec30447de5f39393f24ff9e2
Russell Holz <holzru@gmail.com>
2016-08-03 15:23:31 -0700

    production attemt  3

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,39 @@
 var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/app.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
-  resolve: {
-    extensions: ["", ".js", ".jsx"]
-  },
-  DefinePlugin: {
-  "process.env": {
-     NODE_ENV: JSON.stringify("production")
-    }
-  },
-
-  devtool: 'source-maps',
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   module: {
     loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015']
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
       }
-    },
-    {
-      test: /\.node$/,
-      loader: "node-loader"
-    }
     ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
   }
 };
